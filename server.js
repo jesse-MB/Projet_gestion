b4c7a2e850ebb3047273daf8cdb5adb160bbab2c
jesse-MB <jessemboumba1@gmail.com>
2026-07-23 11:00:00 +0000

    initialisation server nodejs

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+console.log("Bonjour les amis du village , jesse va venir bientot");
+
+app.listen(3000, ()=>{
+console.log("application démarré sur le port 3000");
+});
